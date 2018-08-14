563af0a0919ba842399eb24beed2e13a015b9c78
M. J. Brodzik <brodzik@nsidc.org>
2018-08-14 14:24:47 -0600

    Template toolbox project.

---
+++ tbx/ESPToolbox/Contents.m
@@ -0,0 +1,10 @@
+% ESPToolbox
+% Version 1.0 (R2017b) 14-Aug-2018
+%
+% Classes
+%   myClass  - Perform simple arithmetics.
+%
+% Functions
+%   myfunc   - Multiply input by 2.
+%
+%  Copyright 2018 The MathWorks, Inc.
