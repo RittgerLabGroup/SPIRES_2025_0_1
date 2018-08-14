ede5edb34ecae400a300045d3e7c6c56e9685978
M. J. Brodzik <brodzik@nsidc.org>
2018-08-14 14:36:14 -0600

    Cleaning up first checkin.
    Testing how matlab ide and magit interact.

--- tbx/ESPToolbox/Contents.m
+++ tbx/ESPToolbox/Contents.m
@@ -2,9 +2,10 @@
 % Version 1.0 (R2017b) 14-Aug-2018
 %
 % Classes
-%   myClass  - Perform simple arithmetics.
+
 %
 % Functions
-%   myfunc   - Multiply input by 2.
+
 %
 %  Copyright 2018 The MathWorks, Inc.
+%   getJPLmodis - getJPLmodis retrieves files from JPL MODSCAG website to a local folder
