a61b219c497b437f4f0d672804e566db4becb83b
M. J. Brodzik <brodzik@nsidc.org>
2018-11-12 14:56:05 -0700

    Functions for doing Landsat canopy adjustment.

---
+++ tbx/ESPToolbox/ESPEnv.m
@@ -0,0 +1,83 @@
+classdef ESPEnv
+   properties
+      heightmaskDir
+      modscagDir
+      oliDir
+      viirsDir
+      watermaskDir
+   end
+   methods
+       function obj = ESPEnv()
+           % Default path is relative to this file
+           [path, ~, ~] = fileparts(mfilename('fullpath'));
+           parts = split(path, filesep);
+           path = join(parts(1:end-2), filesep);
+           
+           obj.oliDir = fullfile(path, 'data', 'oli');
+           obj.heightmaskDir = fullfile('/Users', 'brodzik', ...
+               'SierraBighorn_data', 'landcover', 'LandFireEVH_ucsb');
+           obj.modscagDir = fullfile(path, 'data', 'modscag');
+           obj.viirsDir = fullfile(path, 'data', 'viirscag');
+           obj.watermaskDir = fullfile(path, 'data', 'masks');
+
+           % Convert these from 1x1 cells to plain char arrays
+           props = properties(obj);
+           for i = 1:length(props)
+               if iscell(obj.(props{i}))
+                   obj.(props{i}) = obj.(props{i}){1};
+               end
+           end
+        
+       end
+       
+       function f = oliFile(obj, datestr, path, row, varargin)
+           numvarargs = length(varargin);
+           if numvarargs > 1
+               error(sprintf('%s:TooManyInputs, ', ...
+                   'requires at most 1 optional inputs', mfilename()));
+           end
+
+           % fullfile requires char vectors, not modern Strings
+           optargs = {obj.oliDir};
+           optargs(1:numvarargs) = varargin;  
+           [myDir] = optargs{:};
+
+           f = dir(fullfile(myDir, ...
+               sprintf('L8*%s*p%03ir%03i*.mat', datestr, path, row)))
+       end
+       
+       function f = watermaskFile(obj, path, row, varargin)
+           
+           numvarargs = length(varargin);
+           if numvarargs > 1
+               error(sprintf('%s:TooManyInputs, ', ...
+                   'requires at most 1 optional inputs', mfilename()));
+           end
+
+           % fullfile requires char vectors, not modern Strings
+           optargs = {obj.watermaskDir};
+           optargs(1:numvarargs) = varargin;  
+           [myDir] = optargs{:};
+
+           f = dir(fullfile(myDir, ...
+               sprintf('p%03ir%03i_water.tif', path, row)));
+           
+       end
+       
+       function f = heightmaskFile(obj)
+
+           f = dir(fullfile(obj.heightmaskDir, ...
+               'Sierra_utm_LandFire_EVH_gt2.5m_mask.tif'));
+           
+       end
+       
+       function s = heightmask(obj)
+           
+           f = obj.heightmaskFile();
+           s.mask = geotiffread(fullfile(f.folder, f.name));
+           s.info = geotiffinfo(fullfile(f.folder, f.name));
+           
+       end
+       
+   end
+end
