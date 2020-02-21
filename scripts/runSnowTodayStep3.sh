a7c3008e13e0c65fcdb692358f35eb612235c245
M. J. Brodzik <brodzik@nsidc.org>
2020-02-21 11:33:37 -0700

    Adding to SnowToday pipeline up to line plots.

--- scripts/runSnowTodayStep3.sh
+++ scripts/runSnowTodayStep3.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #
-# script to run SnowToday Step2:
-#   update all westernUS daily mosaic files for this year
-#   kick off Step3 for today
+# script to run SnowToday Step3:
+#   update all westernUS SCF_SCD statistics files
+#   for the current year
+#   kick off Step4 for today
 #
 
 #SBATCH --qos normal
 #SBATCH --job-name runSnowTodayStep2
 #SBATCH --account=ucb135_summit1
 #SBATCH --time=00:30:00
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=90G
 #SBATCH --nodes=1
-#SBATCH -o /pl/active/rittger_esp/modis/archive_status/slurm_output/runSnowTodayStep2-%j.out
+#SBATCH -o /pl/active/rittger_esp/modis/archive_status/slurm_output/runSnowTodayStep3-%j.out
 # Set the system up to notify upon completion
 #SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
 #SBATCH --mail-user=brodzik@nsidc.org
@@ -29,13 +31,16 @@ echo "$0: Begin on hostname=$thisHost on $thisDate for yr=$yr and mindays=$minda
 echo "SLURM_SCRATCH=$SLURM_SCRATCH"
 echo "SLURM_JOB_ID=$SLURM_JOB_ID"
 
+threshSCF=10
+threshZ=1200
+
 #Go here so that correct pathdef.m file is used
 cd /projects/brodzik/Documents/MATLAB/esp_SnowToday_ops
 #cd /projects/brodzik/Documents/MATLAB/esp
-matlab -nodesktop -nodisplay -r "clear; updateMosaicFor('westernUS', ${yr}, 'STc', ${mindays}); exit(0);"
+matlab -nodesktop -nodisplay -r "clear; runSummarizeSCA_SCDForLinePlots('westernUS', ${yr}, ${yr}, ${threshSCF}, ${threshZ}, ${mindays}); exit(0);"
 
-#schedule Step 3 to update stats 
-sbatch --dependency=afterok:$SLURM_JOB_ID scripts/runSnowTodayStep3.sh $thisYear $mindays
+#schedule Step 4 to make today's plots
+sbatch --dependency=afterok:$SLURM_JOB_ID scripts/runSnowTodayStep4.sh $thisYear $mindays
 
 thisDate=$(date)
 echo "$0: Done on hostname=$thisHost on $thisDate"
--- scripts/runSnowTodayStep4.sh
+++ scripts/runSnowTodayStep4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# script to run SnowToday Step2:
-#   update all westernUS daily mosaic files for this year
-#   kick off Step3 for today
+# script to run SnowToday Step4:
+#   Make today's SnowToday line and map plots
+#   kick off Step5 for today
 #
 
 #SBATCH --qos normal
@@ -11,7 +11,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH -o /pl/active/rittger_esp/modis/archive_status/slurm_output/runSnowTodayStep2-%j.out
+#SBATCH -o /pl/active/rittger_esp/modis/archive_status/slurm_output/runSnowTodayStep4-%j.out
 # Set the system up to notify upon completion
 #SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
 #SBATCH --mail-user=brodzik@nsidc.org
@@ -29,13 +29,16 @@ echo "$0: Begin on hostname=$thisHost on $thisDate for yr=$yr and mindays=$minda
 echo "SLURM_SCRATCH=$SLURM_SCRATCH"
 echo "SLURM_JOB_ID=$SLURM_JOB_ID"
 
+threshSCF=10
+threshZ=1200
+
 #Go here so that correct pathdef.m file is used
 cd /projects/brodzik/Documents/MATLAB/esp_SnowToday_ops
 #cd /projects/brodzik/Documents/MATLAB/esp
-matlab -nodesktop -nodisplay -r "clear; updateMosaicFor('westernUS', ${yr}, 'STc', ${mindays}); exit(0);"
+matlab -nodesktop -nodisplay -r "clear; todayDt = datetime; plotAnnualSCA_SCDInContext('westernUS', todayDt, ${threshSCF}, ${threshZ}, ${mindays}); exit(0);"
 
-#schedule Step 3 to update stats 
-sbatch --dependency=afterok:$SLURM_JOB_ID scripts/runSnowTodayStep3.sh $thisYear $mindays
+#schedule Step 5 to push plots to NSIDC
+#sbatch --dependency=afterok:$SLURM_JOB_ID scripts/runSnowTodayStep5.sh $creationDate $mindays
 
 thisDate=$(date)
 echo "$0: Done on hostname=$thisHost on $thisDate"
