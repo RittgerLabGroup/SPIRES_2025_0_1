dffe45d7321add475ecf8bd24777fffcd016e359
M. J. Brodzik <brodzik@nsidc.org>
2022-10-18 16:44:03 -0600

    Added variables for sun angles and SCD to output mosaics.

--- scripts/runSnowTodayStep2.sh
+++ scripts/runSnowTodayStep2.sh
@@ -148,6 +148,7 @@ matlab -nodesktop -nodisplay -r "clear; "\
 "mData = MODISData($options); "\
 "varNames={'snow_fraction', 'viewable_snow_fraction', 'grain_size', "\
 "'drfs_grnsz', 'deltavis', 'radiative_forcing', "\
+"'solar_azimuth', 'solar_zenith', "\
 "'snow_cover_days', 'albedo_mu0', 'albedo_muZ'}; "\
 "updateMosaicFor('"$regionName"', "\
 "${yearStart}, ${monthStart}, ${yearStop}, ${monthStop}, "\
