08fd9e5ad0d3e782bc5b3680fa8507adb9d11940
Sebastien Lenard <sebastien.lenard@gmail.com>
2023-06-02 12:06:44 -0600

    runSnowTodayStep0: minor edit

--- scripts/runSnowTodayStep0.sh
+++ scripts/runSnowTodayStep0.sh
@@ -33,7 +33,7 @@
 #SBATCH --time 01:00:00
 #SBATCH --ntasks-per-node 1
 #SBATCH --nodes=1
-#SBATCH -o /scratch/alpine/%u/slurm_out_SnowToday/%x-%A_a.out
+#SBATCH -o /scratch/alpine/%u/slurm_out_SnowToday/%x-%A_%a.out
 # Set the system up to notify upon completion
 # Do not set --mail-user, let it default to the caller
 # It can also be over-written at the command line
