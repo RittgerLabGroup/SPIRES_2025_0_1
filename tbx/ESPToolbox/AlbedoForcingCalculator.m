d13a95ce1aaf8d67fdd91c5464531060e664bf08
Sebastien Lenard <sebastien.lenard@gmail.com>
2024-06-13 14:50:47 -0600

    AlbedoForcingCalculator.m: new allows calculations from lookup tables of albedo/radiative_forcing

---
+++ tbx/ESPToolbox/AlbedoForcingCalculator.m
@@ -0,0 +1,59 @@
+classdef AlbedoForcingCalculator < handle
+  % Calculation of dirty albedo and radiative forcing.
+  % Class designed from the code by Jeff Dozier
+  % https://github.com/DozierJeff/LookupFunctionsSPIReS/blob/main/SnowAlbedoLookupSPIReS.m
+  % https://github.com/DozierJeff/LookupFunctionsSPIReS/blob/main/DarkeningLookupSPIReS.m
+  % retrieved 2024-05-10.
+  properties
+    deltavisGriddedInterpolant  % GridInterpolant obj. To allow lookup table search
+      % of deltavis.
+    dirtyAlbedoGriddedInterpolant  % GridInterpolant obj. To allow lookup table search
+      % of dirty albedo.
+    radiativeForcingGriddedInterpolant  % GridInterpolant obj. To allow lookup table
+      % search of radiative forcing.
+    region % Regions obj.
+  end
+  methods
+    function obj = AlbedoForcingCalculator(region)
+      obj.region = region;
+    end
+    function [dirtyAlbedo, deltavis, radiativeForcing] = getFromLookup(obj, ...
+      grainSize, dustConcentration, solarZenith)
+      % Parameters
+      % ----------
+      % grainSize: int(N dim). Grain size in microns.
+      % dustConcentration: int(N dim). Dust concentration in ppm.
+      % solarZenith: int(N dim). Solar zenith angle in degrees.
+      %
+      % Return
+      % ------
+      % dirtyAlbedo: uint8(N dim). Dirty albedo in percent.
+      % deltavis: uint8(N dim). Deltavis or snow darkening, without units.
+      % radiativeForcing: uin16(N dim). Radiative forcing in W/m2.
+      espEnv = obj.region.espEnv;
+      if isempty(obj.dirtyAlbedoGriddedInterpolant)
+        thisFilePath = espEnv.getFilePathForObjectNameDataLabel('', 'albedolookup');
+        obj.dirtyAlbedoGriddedInterpolant = load(thisFilePath, 'Fdirty').Fdirty;
+      end
+      if isempty(obj.radiativeForcingGriddedInterpolant)
+        thisFilePath = espEnv.getFilePathForObjectNameDataLabel('', 'radiativelookup');
+        obj.deltavisGriddedInterpolant = load(thisFilePath, 'Fdarken').Fdarken;
+        obj.radiativeForcingGriddedInterpolant = load(thisFilePath, 'Fforce').Fforce;
+      end
+      dustFraction = double(dustConcentration) / 1000; % spires dust is in ppm while
+        % AlbedoLookup expects ppt.
+      sootFraction = zeros(size(dustConcentration), 'double');
+      cosOfSolarZenith = cosd(double(solarZenith));
+      cosOfIllumination = cosOfSolarZenith; % based on Jeff ParBal package.
+      dirtyAlbedo = obj.dirtyAlbedoGriddedInterpolant(cosOfSolarZenith, ...
+        cosOfIllumination, sqrt(double(grainSize)), dustFraction, sootFraction);
+        % cosOfSolarZenith: mu0, cosOfIllumination = muI, grainSize: radius,
+        % dustFraction: LAPfraction(1) in Jeff function.
+
+      deltavis = obj.deltavisGriddedInterpolant(cosOfSolarZenith, ...
+        cosOfIllumination, sqrt(double(grainSize)), dustFraction, sootFraction);
+      radiativeForcing = obj.radiativeForcingGriddedInterpolant(cosOfSolarZenith, ...
+        cosOfIllumination, sqrt(double(grainSize)), dustFraction, sootFraction);
+    end
+  end
+end
