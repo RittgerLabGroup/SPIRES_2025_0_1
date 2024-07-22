833e32127fb12991f74b8b4421faab2ccdd137aa
Sebastien Lenard <sebastien.lenard@gmail.com>
2024-07-22 15:16:54 -0600

    configuration.sh: pipeline 2 for USAlaska

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -117,4 +117,36 @@ printf -v pipeLineTasksPerNodeString1 '%s ' ${pipeLineTasksPerNode1[@]}
 printf -v pipeLineMemsString1 '%s ' ${pipeLineMems1[@]}
 printf -v pipeLineTimesString1 '%s ' ${pipeLineTimes1[@]}
 
+########################################################################################
+# Pipeline 2, stopping at mosaic .mats.
+########################################################################################
 
+pipeLineScriptIds2=(mod09gaI spiFillC spiSmooC moSpires scdInCub)
+pipeLineLabels2=(v061 v2024.0d v2024.0d v2024.0d v2024.0d v2024.0d v2024.0d v2024.0d v2024.0d v2024.0d)
+pipeLineRegionTypes2=(0 0 0 0 0 0 1 1 1 10)
+  # 0: tile, 1: big region, 10: all regions.
+pipeLineSequences2=(0 0 001-036 0 0 0 0 0 001-033 0)
+pipeLineSequenceMultiplierToIndices2=(1 1 1 1 1 1 1 1 3 1)
+pipeLineMonthWindows2=(2 2 12 12 12 12 12 0 12 12)
+pipeLineParallelWorkersNb2=(0 18 10 10 0 2 6 0 0 0)
+
+# sbatch parameters
+pipeLineTasksPerNode2=(1 18 10 10 5 2 6 1 1 1)
+pipeLineMems2=(1G 140G 30G 40G 30G 5G 30G 8G 8G 3G)
+pipeLineTimes2=(01:30:00 23:45:00 03:30:00 00:20:00 00:20:00 00:30:00 00:30:00 00:20:00 03:00:00 01:30:00)
+# NB: daGeoBig: time for generation of the last day only.
+# NB: daStatis: time for 3 subdivisions only.
+
+# Bypassing the unavailability of declare -n in bash 4.2.
+# declare -n could have been used in toolsStart.sh to reference these arrays, but
+# it's only available in bash 4.4, while blanca/login/alpine nodes are in bash 4.2
+printf -v pipeLineScriptIdsString2 '%s ' ${pipeLineScriptIds2[@]}
+printf -v pipeLineLabelsString2 '%s ' ${pipeLineLabels2[@]}
+printf -v pipeLineRegionTypesString2 '%s ' ${pipeLineRegionTypes2[@]}
+printf -v pipeLineSequencesString2 '%s ' ${pipeLineSequences2[@]}
+printf -v pipeLineSequenceMultiplierToIndicesString2 '%s ' ${pipeLineSequenceMultiplierToIndices2[@]}
+printf -v pipeLineMonthWindowsString2 '%s ' ${pipeLineMonthWindows2[@]}
+printf -v pipeLineParallelWorkersNbString2 '%s ' ${pipeLineParallelWorkersNb2[@]}
+printf -v pipeLineTasksPerNodeString2 '%s ' ${pipeLineTasksPerNode2[@]}
+printf -v pipeLineMemsString2 '%s ' ${pipeLineMems2[@]}
+printf -v pipeLineTimesString2 '%s ' ${pipeLineTimes2[@]}
