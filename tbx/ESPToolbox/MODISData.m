ccb9a913c6954dfb585e85e9e349e050f66be6ad
Sebastien Lenard <sebastien.lenard@gmail.com>
2025-01-28 17:40:25 -0700

    MODISData: add nrtOrHistoric and algorithm properties, add outputnetcdf/daacnetcdfv202301 labels.

--- tbx/ESPToolbox/MODISData.m
+++ tbx/ESPToolbox/MODISData.m
@@ -7,6 +7,7 @@ classdef MODISData < handle
     %   also used for viirs (controlled by inputProduct properties)             @warning
     %
     properties      % public properties
+        algorithm = 'spires'; % spires (default) or stc.
         archiveDir    % top-level directory with tile data
                         % NB: check hownot to duplicate information with ESPEnv dirs
                         %                                                          @todo
@@ -35,6 +36,7 @@ classdef MODISData < handle
             y0 = 1.000778633335726e+07), tileInfo = struct(dx = 4.633127165279165e+02, ... %4.633127165279169e+02, ...
             dy = 4.633127165279165e+02, columnCount = 2400, rowCount = 2400));
 %}
+        nrtOrHist = 'nrt'; % nrt (default) or hist (historic).
         pixSize_500m = 2 * pi * 6.371007181e+06 / 36 / 2400; %4.633127165279165e+02; %463.31271653; % @deprecated.
         mstruct % mapping structure for MODIS sinusoidal projection
         cstruct % structure with units/fields for MOD09GA files
@@ -113,19 +115,23 @@ classdef MODISData < handle
             % labels can be controlled by the caller
             defaultLabel = 'test';
             checkLabel = @(x) isstring(x) | ischar(x);
+            addParameter(p, 'algorithm', 'spires');
             addParameter(p, 'inputProduct', 'mod09ga');
             addParameter(p, 'inputProductVersion', '061');
             addParameter(p, 'label', defaultLabel, checkLabel);
+            addParameter(p, 'nrtOrHist', 0); % Default nrt.
             addParameter(p, 'versionOfAncillary', obj.defaultVersionOf.ancillary);
 
             p.KeepUnmatched = false;
             parse(p, varargin{:});
 
             label = p.Results.label;
+            obj.algorithm = p.Results.algorithm;
             obj.archiveDir = getenv('espArchiveDir');                      % @deprecated
             obj.inputProduct = p.Results.inputProduct;
 
             obj.inputProductVersion = p.Results.inputProductVersion;
+            obj.nrtOrHist = p.Results.nrtOrHist;
 
             path = fileparts(mfilename('fullpath'));
             parts = split(path, filesep);
@@ -208,6 +214,8 @@ classdef MODISData < handle
                 'VariablesMatlab',  label, ...
                 'variablesmatlab2', label, ...
                 'daacnetcdfv20220', label, ...
+                'daacnetcdfv202301', label, ...
+                'outputnetcdf', label, ...
                 'spiresdailynetcdf', label, ...
                 'spiresfill', label, ...
                 'spiressmoothbycell', label, ...
