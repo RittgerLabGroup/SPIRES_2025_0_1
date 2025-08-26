48823aeb19d2f90ef32b9587512db7cdf2e429ca
Sebastien Lenard <sebastien.lenard@gmail.com>
2025-08-26 12:20:54 -0600

    bash/submitNrt.sh: use of variables inputLabel and outputLabel rather than labels, to prevent confusion with specific step 2 (and 2 for stc).

--- bash/submitNrt.sh
+++ bash/submitNrt.sh
@@ -101,6 +101,7 @@ shift $(($OPTIND - 1))
 
 # External configuration setting.
 ########################################################################################
+export thisEnvironment=${thisEnvironment}
 printf "Load bash/configuration${thisEnvironment}.sh...\n"
 source bash/configuration${thisEnvironment}.sh # include source env/.matlabEnvironmentVariables${thisEnvironment^}
 printf "Load bash/toolsRegions.sh...\n"
@@ -185,13 +186,13 @@ exclude="";
 ########################################################################################
 
 indexInScriptConfigurations=$(echo ${pipeLineScriptIds[@]/${scriptId}//} | cut -d/ -f1 | wc -w | tr -d ' ')
-stepId=$((( $indexInScriptConfigurations + 1)))
-inputLabel=${pipeLineLabels[${indexInScriptConfigurations}]}
-outputLabel=$inputLabel
-indexOfPreLastScript=$((( ${#pipeLineScriptIds[@]} - 1 )))
-if [[ $indexInScriptConfigurations -lt $indexOfPreLastScript && $scriptId != mod09gaI ]]; then
-  outputLabel=${pipeLineLabels[ (( $indexInScriptConfigurations + 1 )) ]}
+
+if [ ${indexInScriptConfigurations} -gt 0 ]; then
+  inputLabel=${pipeLineLabels[$(( ${indexInScriptConfigurations} - 1 ))]}
+else
+  inputLabel=${pipeLineLabels[${indexInScriptConfigurations}]}
 fi
+outputLabel=${pipeLineLabels[${indexInScriptConfigurations}]}
 
 scriptPath=${scriptIdFilePathAssociations[${scriptId}]}
 scriptRegionType=${pipeLineRegionTypes[${indexInScriptConfigurations}]}
